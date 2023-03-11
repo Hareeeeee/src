1523070b8068d08f94b1d99b4752db67696ac94e
Hareee <victor.guiraud@gmail.com>
2023-03-11 14:46:26 +0100

    to the moon

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const routes = require("./routes")
+const app = express()
+app.use(bodyParser.json())
+const port = 3001 // Port of the project
+
+app.set("port", port) // Set the port of the project
+app.use("/", routes)
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
